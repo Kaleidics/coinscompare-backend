25239ce7caab8d84cabf061a9f3f5d9355ea8159
Eddie Chu <contact@eddiechu.me>
2019-03-18 19:43:17 -0700

    change api endpoint to full data

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const api = {
     fetchCoins: function () {
-        const searchUrl = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=100&tsym=USD&api_key=${process.env.API_KEY}`;
+        const searchUrl = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD&api_key=${process.env.API_KEY}`;
         return axios.get(searchUrl)
             .then(response => response)
     }
